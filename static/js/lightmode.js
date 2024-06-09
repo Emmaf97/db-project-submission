78642ef10ba5ebda8421c42ad612c0da14b66eec
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-06-09 18:59:23 +0100

    adding lightmode v1

---
+++ static/js/lightmode.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    let lightToggle = document.getElementById("dark-icon");
+    lightToggle.addEventListener("click", lightMode);
+    checkLightActive();
+}
+
+let toggleCheck = false;
+
+function checkLightActive() {
+    if (localStorage.getItem("light-mode") === "true") {
+        lightMode();
+    }
+}
+
+function lightMode() {
+    let { btnElements, head, nav, pLight, icon, body,  main, foot } = getIndexDocElements();
+    for (let i = 0; i < btnElements.length; i++) {
+        let lightModeAdded = btnElements[i].classList.toggle("dark-icon");
+        if (!lightModeAdded) {
+            toggleCheck = false;
+            localStorage.setItem("light-mode", toggleCheck);
+            body.classList.remove("body-light");
+            head.classList.remove("bg-light")
+            head.classList.add("bg-dark")
+            nav.classList.remove("bg-light")
+            nav.classList.add("bg-dark")
+            icon.classList.add("bg-dark")
+            main.
+            // toplayer.classList.remove("bg-light")
+            // toplayer.classList.add("bg-dark")
+            nav.classList.toggle("bg-light")
+            for (let j = 0; j < pLight.length; j++) {
+                pLight[j].classList.remove("p-light");
+            }
+            for (let c = 0; c < cardtext.length; c++) {
+                cardtext[c].classList.remove("card-text-light")
+            }
+            if (trainergrid){
+            trainergrid.classList.toggle("bg-dark");
+            trainergrid.classList.remove("bg-light");
+            }
+        }
+        else {
+            toggleCheck = true;
+            localStorage.setItem("light-mode", toggleCheck);
+            body.classList.add("body-light");
+            head.classList.remove("bg-dark")
+            head.classList.add("bg-light")
+            nav.classList.remove("bg-dark")
+            nav.classList.add("bg-light")
+            // toplayer.classList.remove("bg-dark")
+            // toplayer.classList.add("bg-light")
+            for (let k = 0; k < pLight.length; k++) {
+                pLight[k].classList.add("p-light");
+            }
+            for (let b = 0; b < cardtext.length; b++) {
+                cardtext[b].classList.add("card-text-light")
+            }
+            if (trainergrid){
+            trainergrid.classList.toggle("bg-dark")
+            trainergrid.classList.add("bg-light")
+            }
+        }
+    }
+}
+function getIndexDocElements() {
+    let btnElements = document.querySelectorAll(".material-symbols-outlined");
+    let body = document.querySelector("body")
+    let head = document.querySelector("header")
+    let nav = document.querySelector("nav")
+    let icon = document.querySelector("icon")
+    let main = document.querySelector("main")
+    let foot = document.querySelector("foot")
+    // let toplayer = document.querySelector(".toplayer")
+    let pLight = document.querySelectorAll(".p.left")
+    let cardtext = document.querySelectorAll(".card-text")
+    // let trainergrid = document.querySelector("#trainer-grid")
+    return { btnElements, body, pLight, cardtext, icon,head, main,nav, foot};
+}
